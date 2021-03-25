10bb3b4d8dc25d2864b2e4a6d7ae47dbdcccc64e
Jorge <djsmoothzouk@gmail.com>
2021-03-25 15:23:03 -0500

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create([
+    {name: "PC Builder", url: "https://pc-builder.netlify.app/", img: "https://res.cloudinary.com/acurunner79/image/upload/v1616702986/Screenshot_from_2021-03-25_15-06-49_jtctip.png", description: "A MERN app that allows a user to build a custom PC with full CRUD routes. This was a group project organized using Airtable and Github branches"}
+])
