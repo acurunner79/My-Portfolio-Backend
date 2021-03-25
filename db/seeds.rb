fde8794ba74b5701d337e58605921cfa5616f4f8
Jorge <djsmoothzouk@gmail.com>
2021-03-25 16:03:29 -0500

    update ruby version

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Project.create([
+p Project.create([
     {name: "PC Builder", url: "https://pc-builder.netlify.app/", img: "https://res.cloudinary.com/acurunner79/image/upload/v1616702986/Screenshot_from_2021-03-25_15-06-49_jtctip.png", description: "A MERN app that allows a user to build a custom PC with full CRUD routes. This was a group project organized using Airtable and Github branches"}
 ])
